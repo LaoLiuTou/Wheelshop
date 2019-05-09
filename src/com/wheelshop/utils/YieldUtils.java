2e8e22e64683416d9ea6535b2c4d046aca0dcee3
TongTong <369259353@qq.com>
2019-05-09 11:52:14 +0800

    com

--- src/com/wheelshop/utils/YieldUtils.java
+++ src/com/wheelshop/utils/YieldUtils.java
@@ -1,6 +1,10 @@
 package com.wheelshop.utils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -11,17 +15,19 @@ import org.springframework.stereotype.Component;
 
 import com.wheelshop.model.production.Production;
 import com.wheelshop.service.production.IProductionService;
+import com.wheelshop.service.timer.ITimerService;
 @Component
 public class YieldUtils {
 	 
 	private static IProductionService iProductionService;
-	    
+	private static ITimerService iTimerService;
     @Autowired
-    public YieldUtils(IProductionService iProductionService) {
+    public YieldUtils(IProductionService iProductionService,ITimerService iTimerService) {
     	YieldUtils.iProductionService = iProductionService;
+    	YieldUtils.iTimerService = iTimerService;
     }
 	private static Map<String,Timer> timerMap = new HashMap<String,Timer>();
-	public static void startSchedule(final String id,String prodnum,String interval){
+	public static void startSchedule(final String id,final String prodnum,String interval){
 		Logger logger = Logger.getLogger("WheelshopLogger");
 		Timer timer = new Timer();
 		
@@ -36,12 +42,41 @@ public class YieldUtils {
 		logger.info("计时器总数:"+timerMap.size());
 		logger.info("开始计时，生产线："+prodnum+";时间间隔："+interval);
 	    timer.scheduleAtFixedRate(new TimerTask() {
-	      public void run() {
-	    	  Production temp=iProductionService.selectProductionById(id);
-	    	  Production p=new Production();
-	    	  p.setId(Long.parseLong(id));
-	    	  p.setYield((Integer.parseInt(temp.getYield())+1)+"");
-	    	  iProductionService.updateProduction(p);
+	    @SuppressWarnings({ "unchecked", "rawtypes" })
+		public void run() {
+	    	  
+	    	  try {
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				  SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd");
+					
+				  Map paramMap=new HashMap();
+				  paramMap.put("starttime",sdf.format(new Date()));
+				  paramMap.put("prodnum",prodnum); 
+				  int count=iTimerService.selectCountTimerByParam(paramMap);
+				  paramMap.put("fromPage",0);
+				  paramMap.put("toPage",count); 
+				  List<com.wheelshop.model.timer.Timer> list=iTimerService.selectTimerByParam(paramMap);
+				  Date currentDate=new Date();
+				  boolean flag = true;
+				  for(com.wheelshop.model.timer.Timer item:list){
+					  String starttimes=sdf2.format(currentDate)+" "+item.getStarttime();
+					  String endtimes  =sdf2.format(currentDate)+" "+item.getEndtime();
+					  if(currentDate.after(sdf.parse(starttimes))
+							  &&currentDate.before(sdf.parse(endtimes))){
+						  flag=false;
+					  }
+				  }
+				  if(flag){
+					  Production temp=iProductionService.selectProductionById(id);
+					  Production p=new Production();
+					  p.setId(Long.parseLong(id));
+					  p.setYield((Integer.parseInt(temp.getYield())+1)+"");
+					  iProductionService.updateProduction(p);
+				  }
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	      }
 	    }, (int)(Float.parseFloat(interval)*1000),  (int)(Float.parseFloat(interval)*1000));
 	}
